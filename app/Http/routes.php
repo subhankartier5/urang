0b51145ed1fb8ae51af3622657c2d0442216c759
Localhost <work@tier5.us>
2016-06-16 11:27:11 +0530

    changes in routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,16 +14,16 @@
 Route::get('/',['uses' => 'MainController@getIndex', 'as' => 'index']);
 
 /*Admin Routes*/
-Route::get('admin', ['uses' => 'AdminController@index', 'as' => 'get-admin-login']);
-Route::post('admin', ['uses' => 'AdminController@LoginAttempt', 'as' => 'post-admin-login']);
-Route::get('dashboard', ['uses' => 'AdminController@getDashboard', 'middleware' => 'auth', 'as' => 'get-admin-dashboard']);
-Route::get('logout', ['uses' => 'AdminController@logout', 'as' => 'get-admin-logout']);
-Route::get('profile-details', ['uses' => 'AdminController@getProfile', 'as' => 'get-admin-profile']);
-Route::post('profile-details', ['uses' => 'AdminController@postProfile', 'as' => 'post-admin-profile']);
-Route::get('settings', ['uses' => 'AdminController@getSettings', 'as' => 'get-admin-settings']);
-Route::post('change-password', ['uses' => 'AdminController@postChangePassword', 'as' => 'post-change-password']);
-Route::post('site-settings', ['uses' => 'AdminController@postSiteSettings', 'as' => 'post-site-settings']);
-Route::get('neighborhood', ['uses' => 'AdminController@getNeighborhood', 'as' => 'get-neighborhood']);
-Route::post('neighborhood', ['uses' => 'AdminController@postNeighborhood', 'as' => 'postneighborhood' ]);
-Route::post('edit-neighborhood', ['uses' => 'AdminController@editNeighborhood', 'as' => 'editneighborhood']);
-Route::post('delete-neighborhood', ['uses' => 'AdminController@deleteNeighborhood', 'as' => 'postDeleteNeighborhood']);
+Route::get('/admin', ['uses' => 'AdminController@index', 'as' => 'get-admin-login']);
+Route::post('/admin', ['uses' => 'AdminController@LoginAttempt', 'as' => 'post-admin-login']);
+Route::get('/dashboard', ['uses' => 'AdminController@getDashboard', 'middleware' => 'auth', 'as' => 'get-admin-dashboard']);
+Route::get('/logout', ['uses' => 'AdminController@logout', 'as' => 'get-admin-logout']);
+Route::get('/profile-details', ['uses' => 'AdminController@getProfile', 'as' => 'get-admin-profile']);
+Route::post('/profile-details', ['uses' => 'AdminController@postProfile', 'as' => 'post-admin-profile']);
+Route::get('/settings', ['uses' => 'AdminController@getSettings', 'as' => 'get-admin-settings']);
+Route::post('/change-password', ['uses' => 'AdminController@postChangePassword', 'as' => 'post-change-password']);
+Route::post('/site-settings', ['uses' => 'AdminController@postSiteSettings', 'as' => 'post-site-settings']);
+Route::get('/neighborhood', ['uses' => 'AdminController@getNeighborhood', 'as' => 'get-neighborhood']);
+Route::post('/neighborhood', ['uses' => 'AdminController@postNeighborhood', 'as' => 'postneighborhood' ]);
+Route::post('/edit-neighborhood', ['uses' => 'AdminController@editNeighborhood', 'as' => 'editneighborhood']);
+Route::post('/delete-neighborhood', ['uses' => 'AdminController@deleteNeighborhood', 'as' => 'postDeleteNeighborhood']);
