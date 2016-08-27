1dc35bc65811a0bcf29853655a9c63046f1319c7
tier511 <work@tier5.us>
2016-08-27 14:31:57 +0530

    email on complaint v2

--- resources/views/email/complaints.blade.php
+++ resources/views/email/complaints.blade.php
@@ -123,7 +123,7 @@
             <table cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="max-width: 600px;" role="presentation">
 				<tr>
 					<td style="padding: 20px 0; text-align: center">
-						<img src="{{url('/')}}/public/images/logo.png" width="200" height="50" alt="u-rang logo" border="0">
+						<img src="http://162.243.64.194/public/new/img/logo-white.png" width="200" height="50" alt="u-rang logo" border="0">
 					</td>
 				</tr>
             </table>
@@ -135,7 +135,7 @@
                 <!-- Hero Image, Flush : BEGIN -->
                 <tr>
 					<td bgcolor="#ffffff">
-						<img src="{{url('/')}}/public/images/com.png" width="600" height="" alt="alt_text" border="0" align="center" style="width: 100%; max-width: 600px;">
+						<img src="http://162.243.64.194/public/images/com.png" width="600" height="" alt="alt_text" border="0" align="center" style="width: 100%; max-width: 600px;">
 					</td>
                 </tr>
                 <!-- Hero Image, Flush : END -->
@@ -151,7 +151,7 @@
 	                            			<div class="row">
 	                            				<div class="col-xs-5"></div>
 	                            				<div class="col-xs-2">
-	                            					<img src="{{url('/')}}/public/images/cus_icon.png" alt="customer icon" style="height: 60px;margin-left: 222px;margin-top: 10px;">	
+	                            					<img src="http://162.243.64.194/public/images/cus_icon.png" alt="customer icon" style="height: 60px; width:60px; margin-left: 222px;margin-top: 10px;">	
 	                            				</div>
 	                            				<div class="col-xs-5"></div>
 	                            			</div>
@@ -192,7 +192,7 @@
                                     <table cellspacing="0" cellpadding="0" border="0" width="100%" style="font-size: 14px;text-align: left;">
                                         <tr>
                                             <td style="text-align: center; padding: 0 10px;">
-                                                <img src="{{url('/')}}/public/images/com_small.png" alt="complaint_logo" style="height: 100px;" class="center-on-narrow">
+                                                <img src="http://162.243.64.194/public/images/com_small.png" alt="complaint_logo" style="height: 100px;" class="center-on-narrow">
                                             </td>
                                         </tr>
                                         <tr>
