d472f9e0e29a491a70601fcdf75356031d432710
tier511 <work@tier5.us>
2016-08-27 14:15:30 +0530

    email sender on complaint v1.1

--- app/Listeners/CustomerComplaintsListener.php
+++ app/Listeners/CustomerComplaintsListener.php
@@ -30,7 +30,7 @@ class CustomerComplaintsListener
         Mail::send('email.complaints', array('first_name'=> $event->req->firstName,'last_name'=> $event->req->lastName, 'phone' => $event->req->phone, 'email'=>$event->req->email, 'subject' => $event->req->subject, 'complaint' => $event->req->message), 
             function($message) use ($event)
             {
-            $message->from($event->req->firstName." ". $event->req->lastName);
+            $message->from($event->req->email);
             $message->to("work@tier5.us", $event->req->firstName)->subject($event->req->subject);
         });
         //dd($event->req);
